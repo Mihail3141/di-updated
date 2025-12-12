9d620496b9e5378ebd145b4b0e4c3cf35261da32
MihailK <mihailkopitov@mail.ru>
2025-12-12 19:22:23 +0500

    Добавил контейнер

--- TagsCloudContainer/Program.cs
+++ TagsCloudContainer/Program.cs
@@ -1,25 +1,60 @@
 ﻿using System.Drawing;
+using Autofac;
 using TagsCloudContainer.TagBuilder;
 using TagsCloudContainer.TagsCloudVisualization.Render;
 using TagsCloudContainer.TextReader;
 using TagsCloudContainer.WordProcess;
-using TagsCloudVisualization;
+using TagsCloudVisualization.CircularCloudLayouter;
 using TagsCloudVisualization.PointGenerator;
 
 namespace TagsCloudContainer;
 
 public static class Program
 {
+    private static IContainer CreateContainer()
+    {
+        var builder = new ContainerBuilder();
+        var center = new Point(1920 / 2, 1080 / 2);
+        builder.RegisterType<Reader>().AsSelf().SingleInstance();
+        builder.Register(ctx =>
+            new WordProcessor());
+        builder.Register(ctx =>
+            new SpiralPointGenerator(center, 1, double.Pi / 24));
+        builder.Register(ctx =>
+                new CircularCloudLayouter(center, 20000, ctx.Resolve<SpiralPointGenerator>()))
+            .As<ICloudLayouter>().SingleInstance();
+        builder.Register(ctx => new TagCloudBuilder(ctx.Resolve<ICloudLayouter>()))
+            .As<ITagCloudBuilder>().SingleInstance();
+        builder.Register(ctx => new TagCloudRenderSettings
+            {
+                ImageSize = new Size(1920, 1080),
+                FontName = "Times New Roman",
+                FontStyle = FontStyle.Bold,
+                FontSizeMultiplier = 0.6f
+            })
+            .AsSelf().SingleInstance();
+        builder.Register(ctx =>
+                new TagCloudRenderer(ctx.Resolve<TagCloudRenderSettings>()))
+            .As<ICloudRenderer>().SingleInstance();
+
+        return builder.Build();
+    }
+
     private static void Main()
     {
-        var reader = new Reader();
-        var wordProcessor = new WordProcessor();
+        var container = CreateContainer();
+        using var scope = container.BeginLifetimeScope();
+        
+        var reader = scope.Resolve<Reader>();
+        var wordProcessor = scope.Resolve<WordProcessor>();
+        var builder = scope.Resolve<ITagCloudBuilder>();
+        var renderer = scope.Resolve<ICloudRenderer>();
 
         var sourceFilePath = Path.Combine("..", "..", "..", "SourceFile", "ГарриПоттерНаРусском.txt");
-        
+
         var wordsFromText = reader.GetWordsFromFile(sourceFilePath);
         var processedWords = wordProcessor
-            .SetAllowedPartsOfSpeech(["S","V"])
+            .SetAllowedPartsOfSpeech(["S", "V"])
             .ExcludeWords(["быть"])
             .AddWordFilter(word => word.Lemma.Length > 2)
             .Get(wordsFromText);
@@ -27,28 +62,13 @@ public static class Program
         // var sourceFileDir = Path.GetDirectoryName(sourceFilePath) ?? "SourceFile";
         // var saver = new LemmasSaver(sourceFileDir);
         // saver.SaveLemmas(result);
-        
+
         var frequency = processedWords
             .GroupBy(w => w)
             .ToDictionary(g => g.Key, g => g.Count());
         
-        
-        var center = new Point(1920 / 2, 1080 / 2);
-        var pointGenerator = new SpiralPointGenerator(center, 1, double.Pi / 24);
-        var layouter = new CircularCloudLayouter(center, 20000, pointGenerator);
-
-        var builder = new TagCloudBuilder(layouter);
         var tags = builder.BuildTagCloud(100, frequency, 5, 120);
-
-        var settings = new TagCloudRenderSettings
-        {
-            ImageSize = new Size(1920, 1080),
-            FontName = "Arial",
-            FontStyle = FontStyle.Italic,
-            FontSizeMultiplier = 0.5f 
-        };
-        var renderer = new TagCloudRenderer(settings);
-
+        
         using var bitmap = renderer.CreateRectangleCloud(tags);
         bitmap.Save("tagcloud.png", System.Drawing.Imaging.ImageFormat.Png);
         Console.WriteLine("Облако сохранено: tagcloud.png");
